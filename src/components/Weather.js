e7bca2aef345193e98683b4a5b240cb735ffeb07
Ninad Bhoir <ninadbhoir010@gmail.com>
2022-11-24 11:41:59 +0530

    final pushing

---
+++ src/components/Weather.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./weather.css";
+const Weather = () => {
+  //getting data here
+  const [data, setData] = useState({});
+  //getting location from user
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  // fetching api
+  const searchLocation = async (e) => {
+    if (e.key == "Enter") {
+      const data = await fetch(url);
+      const jsonData = await data.json();
+      setData(jsonData); //setting json data into data
+      console.log("data", jsonData);
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed(0)}°Cel</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        <div className="wave"></div> {/*wave is taken for animation*/}
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°Cel</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+    // we use ternary operator for ensuring that we are fetching data and saving into the state. if data is not available due to some api reason then we will get null value
+  );
+};
+
+export default Weather;
